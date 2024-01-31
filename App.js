401d3e69bb78c2cc75928b47c14db04108d9feb6
theabhinavarya <anuragarya159@gmail.com>
2024-02-01 00:00:08 +0530

    episode-3

--- App.js
+++ App.js
@@ -5,8 +5,14 @@ import ReactDOM from "react-dom/client";
 
 const heading = React.createElement("h1", {id: "heading"}, "Nameste React"); 
 
+// JSX
+const jsxHeading = <h1 id="heading">Nameste JSX </h1>;
+
 console.log(heading);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+console.log(jsxHeading);
+
+
+root.render(jsxHeading);
